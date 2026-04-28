3d75b8fc94dbb3e6dce78549c64f587a1843ef70
pranaybhoir <pranaytb777@gmail.com>
2026-04-28 13:04:40 +0530

    Initialize Maven Wrapper and add project essentials including `.gitignore`, `.gitattributes`, and base Spring Boot application setup.

---
+++ POC2/src/test/java/com/pranay/spring/security/poc2/Poc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.spring.security.poc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
