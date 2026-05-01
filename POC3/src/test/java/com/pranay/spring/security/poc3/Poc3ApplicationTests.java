7fc207e87b4eaa7b9822d962f613906ba2eeacf3
pranaybhoir <pranaytb777@gmail.com>
2026-05-01 17:44:15 +0530

    Initialize project structure with Maven Wrapper, base Spring Boot application, and essential configurations.

---
+++ POC3/src/test/java/com/pranay/spring/security/poc3/Poc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.spring.security.poc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
