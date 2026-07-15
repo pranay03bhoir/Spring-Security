d110688be69574a84e0ade58fab3b306fbdbbe9a
pranaybhoir <pranaytb777@gmail.com>
2026-07-15 16:25:06 +0530

    Initialize Spring Boot application with Maven wrapper and basic project structure

---
+++ POC5/src/test/java/com/pranay/spring/security/poc5/Poc5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.spring.security.poc5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
